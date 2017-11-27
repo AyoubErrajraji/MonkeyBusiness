3faac7424f31f8a20d82530e19325b72912d2582
Luke Hol <lukehol2000@gmail.com>
2017-11-27 11:33:24 +0100

    addded data.json

--- crosstheroad_lib/crosstheroad.py
+++ crosstheroad_lib/crosstheroad.py
@@ -1,1 +1,15 @@
-print("Hello")
+import pygame, sys, time, json
+from math import *
+from pygame.locals import *
+
+pygame.font.init()
+
+white = (255, 255, 255)
+black = (0, 0, 0)
+blue = (0, 255, 0)
+red = (255, 0, 0)
+green = (0, 200, 0)
+yellow = (255, 255, 0)
+
+screenDim = json.load(open('./data/'))
+screen = pygame.display.set_mode(screenDim)
