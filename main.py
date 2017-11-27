bc8a839692145fd81b5002498d4074086ee443af
Richard <richard.vd.knaap@outlook.com>
2017-11-27 10:28:29 +0100

    naar mijn eigen branch

---
+++ main.py
@@ -0,0 +1,15 @@
+'''
+Created on Nov 25, 2017
+@author: lexdewilligen
+'''
+from menu_lib import slidemenu
+from bananattack_lib import bananattack
+
+if __name__ == "__main__":
+
+    mymenu = slidemenu.run()
+    mymenu.runm()
+
+    # Manually minigame runnen
+    #mygame = bananattack.run()
+    #mygame.runm()
