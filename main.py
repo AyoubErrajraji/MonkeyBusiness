da16d3d1428e07e8e0edd10c5b6dc62fd3891d0a
Lex de Willigen <lexdewillligen@outlook.com>
2017-11-27 10:53:34 +0100

    Updated menu

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from menu_lib import slidemenu
 from bananattack_lib import bananattack
 
 if __name__ == "__main__":
-
     mymenu = slidemenu.run()
     mymenu.runm()
 
     # Manually minigame runnen
-    #mygame = bananattack.run()
-    #mygame.runm()
+    # mygame = bananattack.run()
+    # mygame.runm()
+
+
