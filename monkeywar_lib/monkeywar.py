f175d16a2b79b7df77b7e5f707a883a72194b29a
Richard <richard.vd.knaap@outlook.com>
2017-11-28 17:44:55 +0100

    push naar mijn eigen zooi

--- monkeywar_lib/monkeywar.py
+++ monkeywar_lib/monkeywar.py
@@ -11,7 +11,8 @@ RED = (255, 0, 0)
 GREEN = (0, 200, 0)
 BRIGHT_GREEN = (0, 255, 0)
 SAND = (255,255,100)
-GREY = (182, 179, 179)
+GREY = (155,155,155)
+
 
 surface = pygame.display.set_mode((1280,720))
 
@@ -27,9 +28,16 @@ class run(object):
         duration = 5000
         win = projectWin(500, 500, 'MonkeyWar')
         Quit = False
+
+        secondMonkey = Monkey(1080, "data/monkeywar/tankl.png", "ARROWS")
+        firstMonkey = Monkey(100, "data/monkeywar/tankr.png","WASD")
+
+
+
         while not Quit:
             # get Mouse
             mouse = pygame.mouse.get_pos()
+
             # set Background
             surface.blit(pygame.transform.scale(pygame.image.load('data/monkeywar/bg.jpg').convert(), (1280, 720)),
                          (0, 0))
@@ -37,9 +45,16 @@ class run(object):
             #call classes
             win.ground()
 
-            # Display the res
+            secondMonkey.move()
+            firstMonkey.move()
+
+            # update display
             pygame.display.update()
+
+
+            # Display the res
             time.sleep(0.02)
+
 
             # Quit handler
             for event in pygame.event.get():
@@ -65,10 +80,60 @@ class projectWin:
         self.height = height
         self.placeHolders = []
         self.placeHolders.append(PlaceHolder(surface, 1, 236, 10, 10, GREEN))
-        #placeHolder1 = PlaceHolder(surface, 10, 236, 10, 10, GREEN)
 
     def ground(self):
-        pygame.draw.rect(surface, GREY, (0, 565, 1270, 165), 0)
+        pygame.draw.rect(surface, GREY, (0, 550, 1280, 170), 0)
+
+
+class Monkey(object):
+    def __init__(self, x, image, movement):
+        self.x = x
+        self.image = image
+        self.movement = movement
+
+    def draw(self, position):
+        sprite = pygame.image.load(self.image).convert_alpha()
+        surface.blit(pygame.transform.scale(sprite, (110, 80)), position)
+
+    def move(self):
+        pygame.event.pump()
+
+        # a key has been pressed
+        keyinput = pygame.key.get_pressed()
+
+        # press escape key to quit game
+        if keyinput[pygame.K_ESCAPE]:
+            raise SystemExit
+        # optional exit on window corner x click
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                raise SystemExit
+
+        # check arrow keys
+        # move sprite in direction of arrow by 2 pixels
+
+        if self.movement == "ARROWS":
+
+            if keyinput[pygame.K_LEFT]:
+                self.x -= 10
+            elif keyinput[pygame.K_RIGHT]:
+                self.x += 10
+
+        if self.movement == "WASD":
+            if keyinput[pygame.K_a]:
+                self.x -= 10
+            elif keyinput[pygame.K_d]:
+                self.x += 10
+
+        self.draw((self.x,478))
+
+
+
+
+
+
+
 
 
 
