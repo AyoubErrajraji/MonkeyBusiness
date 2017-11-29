3731c0f7a5f1d56573b9656570cdd42f4eea6dce
Lex de Willigen <lexdewillligen@outlook.com>
2017-11-29 16:53:12 +0100

    Toch maar wel main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+Created on Nov 25, 2017
+@author: lexdewilligen
+'''
+from menu_lib import slidemenu
+from bananattack_lib import main as bananattack
+
+mymenu = slidemenu.run()
+mymenu.runm()
+
+# Manually minigame runnen
+#mygame = bananattack.main()
+
+
+
+
