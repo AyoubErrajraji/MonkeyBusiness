2d6af9f18af761807acaa92034d45bfaee704374
AyoubErrajraji <33752416+AyoubErrajraji@users.noreply.github.com>
2017-12-06 16:49:59 +0100

    added classes
    -updated class player
    -added class boss
    -added class wolk

--- finalfight_lib/test.py
+++ finalfight_lib/test.py
@@ -36,10 +36,57 @@ class Player(Game):
         playerHeight = self.player.get_rect().height
         self.player = pygame.transform.scale(self.player, (playerWidth, playerHeight))
 
+    def blitPlayer(self):
+        self.screen.blit(self.player, (200, 550))
+
+    def movePlayer(self):
+        """ Handles Keys """
+        key = pygame.key.get_pressed()
+        dist = 1  # distance moved in 1 frame, try changing it to 5
+        if key[pygame.K_DOWN]:  # down key
+            self.y += dist  # move down
+        elif key[pygame.K_UP]:  # up key
+            self.y -= dist  # move up
+        if key[pygame.K_RIGHT]:  # right key
+            self.x += dist  # move right
+        elif key[pygame.K_LEFT]:  # left key
+            self.x -= dist  # move left
+
+    def draw(self, surface):
+        """ Draw on surface """
+        # blit yourself at your current position
+        surface.blit(self.image, (self.x, self.y))
+
+class Boss(Game):
+    def __init__(self,screen):
+        self.bossX = 350
+        self.bossY = 300
+        self.boss = None
+
+    def loadBoss(self,name):
+        self.boss = pygame.image.load("boss2.png").convert_alpha()
+        bossWidth = self.boss.get_rect().width
+        bossHeight = self.boss.get_rect().height
+        self.boss = pygame.transform.scale(self.boss, (bossWidth, bossHeight))
+
+    def blitBoss(self):
+        screen.blit(boss, (350, 300))
 
 
+class Wolk(Game):
+    def __init__(self, screen):
+        self.wolkX = 430
+        self.wolkY = 130
+        self.wolk = None
 
+    def loadWolk(self, name):
+        self.wolk = pygame.image.load("boss2.png").convert_alpha()
+        wolkWidth = self.wolk.get_rect().width
+        wolkHeight = self.wolk.get_rect().height
+        self.wolk = pygame.transform.scale(self.wolk, (wolkWidth,wolkHeight))
 
+    def blitwolk(self):
+        screen.blit(self.wolk, (250, 200))
 
 
 width = 900
