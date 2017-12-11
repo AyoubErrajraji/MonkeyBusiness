5a13ae86033d161f40ece4c98aaf562aceed8938
AyoubErrajraji <33752416+AyoubErrajraji@users.noreply.github.com>
2017-12-11 09:38:10 +0100

    implemented score function

--- finalfight_lib/game.py
+++ finalfight_lib/game.py
@@ -3,14 +3,14 @@ import sys
 import json
 
 def getMemory(key):
-    with open("memory.json", "r+") as jsonFile:
+    with open("finalfight_lib/memory.json", "r+") as jsonFile:
         data = json.load(jsonFile)
 
         return data[key]
 
 
 def setMemory(key, value):
-    with open("memory.json", "r+") as jsonFile:
+    with open("finalfight_lib/memory.json", "r+") as jsonFile:
         data = json.load(jsonFile)
 
         data[key] = value
@@ -19,56 +19,57 @@ def setMemory(key, value):
         json.dump(data, jsonFile)
         jsonFile.truncate()
 
+class run():
+    def runm(self):
+        width = 1280
+        height = 720
+        screenDim = (width, height)
 
-width = 1280
-height = 720
-screenDim = (width, height)
+        screen = pygame.display.set_mode(screenDim)
 
-screen = pygame.display.set_mode(screenDim)
+        pygame.display.set_caption("Final Fight")
 
-pygame.display.set_caption("Final Fight")
+        forrestImage = pygame.image.load("data/finalfight/darkForrest.jpg").convert()
+        forrestImage = pygame.transform.scale(forrestImage,(width,height))
+        screen.blit(forrestImage,(0,0))
 
-forrestImage = pygame.image.load("darkForrest.jpg").convert()
-forrestImage = pygame.transform.scale(forrestImage,(width,height))
-screen.blit(forrestImage,(0,0))
+        rescale = 2
+        boss = pygame.image.load("data/finalfight/boss2.png").convert_alpha()
+        bossWidth = boss.get_rect().width
+        bossHeight = boss.get_rect().height
+        boss = pygame.transform.scale(boss,(bossWidth,bossHeight))
+        screen.blit(boss,(350,300))
 
-rescale = 2
-boss = pygame.image.load("boss2.png").convert_alpha()
-bossWidth = boss.get_rect().width
-bossHeight = boss.get_rect().height
-boss = pygame.transform.scale(boss,(bossWidth,bossHeight))
-screen.blit(boss,(350,300))
+        wolk = pygame.image.load("data/finalfight/spreekwolk.png").convert_alpha()
+        wolkWidth = wolk.get_rect().width
+        wolkHeight = wolk.get_rect().height
+        wolk = pygame.transform.scale(wolk,(250,200))
+        screen.blit(wolk,(430,130))
 
-wolk = pygame.image.load("spreekwolk.png").convert_alpha()
-wolkWidth = wolk.get_rect().width
-wolkHeight = wolk.get_rect().height
-wolk = pygame.transform.scale(wolk,(250,200))
-screen.blit(wolk,(430,130))
-
-player = pygame.image.load("monkey.png").convert_alpha()
-playerWidth = player.get_rect().width
-playerHeight = player.get_rect().height
-player = pygame.transform.scale(player,(150,150))
-screen.blit(player,(200,550))
+        player = pygame.image.load("data/finalfight/monkey.png").convert_alpha()
+        playerWidth = player.get_rect().width
+        playerHeight = player.get_rect().height
+        player = pygame.transform.scale(player,(150,150))
+        screen.blit(player,(200,550))
 
 
-pygame.font.init()
-font = pygame.font.Font("../data/finalfight/FEASFBRG.ttf", 30)
-score = "score: %d" % getMemory("score")
-temp_surface = font.render(score, 1, (255,255,255))
-screen.blit(temp_surface,(1150,10))
+        pygame.font.init()
+        font = pygame.font.Font("data/finalfight/FEASFBRG.ttf", 30)
+        score = "score: %d" % getMemory("score")
+        temp_surface = font.render(score, 1, (255,255,255))
+        screen.blit(temp_surface,(1150,10))
 
-setMemory("score", 768)
+        setMemory("score", 768)
 
-finished = False
+        finished = False
 
-while not finished:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            finished = True
-            pygame.quit()
-            sys.exit()
-        pygame.display.flip()
+        while not finished:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    finished = True
+                    pygame.quit()
+                    sys.exit()
+                pygame.display.flip()
 
 
 
