72da2e70ac8eea25e65e7695348465a5438d2993
AyoubErrajraji <33752416+AyoubErrajraji@users.noreply.github.com>
2017-12-15 11:33:54 +0100

    implemented classes and linked it to menu
    changed code into classes and switched
    "game.py" with "test.py"

--- finalfight_lib/game.py
+++ finalfight_lib/game.py
@@ -4,54 +4,151 @@ import json
 import os
 import math
 import itertools
+pygame.init()
+pygame.font.init()
 
 
-def getMemory(key):
-    with open("finalfight_lib/memory.json", "r+") as jsonFile:
-        data = json.load(jsonFile)
+class Game:
+    def __init__(self,screen):
+        self.fps = 30
+        self.frame = pygame.time.Clock()
+        self.screen = screen
 
-        return data[key]
+    def updateFrame(self):
+        self.frame.tick(self.fps)
+        pygame.display.flip()
+
+class Background(Game):
+    def __init__(self,screen):
+        Game.__init__(self,screen)
+        self.rescale = 2
+        self.forrestImage = None
+
+    def loadForrest(self,name):
+        self.forrestImage = pygame.image.load(name).convert()
+        self.forrestImage = pygame.transform.scale(self.forrestImage, (1280, 720))
+
+    def blitForrest(self):
+        self.screen.blit(self.forrestImage, (0, 0))
+
+class Player(Game):
+    def __init__(self,screen):
+        Game.__init__(self,screen)
+        self.playerX = 200
+        self.playerY = 550
+        self.player = None
+
+    def loadPlayer(self,name):
+        self.player = pygame.image.load(name).convert_alpha()
+        playerWidth = self.player.get_rect().width
+        playerHeight = self.player.get_rect().height
+        self.player = pygame.transform.scale(self.player, (150, 150))
+
+    def blitPlayer(self):
+        self.screen.blit(self.player, (300, 550))
+
+    def movePlayer(self):
+        """ Handles Keys """
+        key = pygame.key.get_pressed()
+        dist = 1  # distance moved in 1 frame, try changing it to 5
+        if key[pygame.K_DOWN]:  # down key
+            self.y += dist  # move down
+        elif key[pygame.K_UP]:  # up key
+            self.y -= dist  # move up
+        if key[pygame.K_RIGHT]:  # right key
+            self.x += dist  # move right
+        elif key[pygame.K_LEFT]:  # left key
+            self.x -= dist  # move left
+
+    #def draw(self, screen):
+        #""" Draw on surface """
+        # blit yourself at your current position
+        #screen.blit(self.image, (self.x, self.y))
+
+class Boss(Game):
+    def __init__(self,screen):
+        Game.__init__(self, screen)
+        self.bossX = 350
+        self.bossY = 300
+        self.boss = None
+
+    def loadBoss(self,name):
+        self.boss = pygame.image.load(name).convert_alpha()
+        bossWidth = self.boss.get_rect().width
+        bossHeight = self.boss.get_rect().height
+        self.boss = pygame.transform.scale(self.boss, (bossWidth, bossHeight))
+
+    def blitBoss(self,screen):
+        screen.blit(self.boss, (520, 300))
 
 
-def setMemory(key, value):
-    with open("finalfight_lib/memory.json", "r+") as jsonFile:
-        data = json.load(jsonFile)
+class Wolk(Game):
+    def __init__(self, screen):
+        Game.__init__(self, screen)
+        self.wolkX = 430
+        self.wolkY = 130
+        self.wolk = None
 
-        data[key] = value
+    def loadWolk(self, name):
+        self.wolk = pygame.image.load(name).convert_alpha()
+        wolkWidth = self.wolk.get_rect().width
+        wolkHeight = self.wolk.get_rect().height
+        self.wolk = pygame.transform.scale(self.wolk, (350,200))
 
-        jsonFile.seek(0)  # rewind
-        json.dump(data, jsonFile)
-        jsonFile.truncate()
+    def blitwolk(self,screen):
+        screen.blit(self.wolk, (590, 130))
+
+class Score(Game):
+    def __init__(self,screen):
+        Game.__init__(self,screen)
 
 
-#class Player(object):  # represents the bird, not the game
-   # def __init__(self):
- #       """ The constructor of the class """
-  #      self.image = pygame.image.load('data/finalfight/monkey.png')
-   #     # the bird's position
-    #    self.x = 0
-     #   self.y = 0
+    def getMemory(key):
+        with open("finalfight_lib/memory.json", "r+") as jsonFile:
+            data = json.load(jsonFile)
 
-    #def handle_keys(self):
-     #   """ Handles Keys """
-      #  key = pygame.key.get_pressed()
-       # dist = 1 # distance moved in 1 frame, try changing it to 5
-        #if key[pygame.K_DOWN]: # down key
-         #   self.y += dist # move down
-        #elif key[pygame.K_UP]: # up key
-        #    self.y -= dist # move up
-        #if key[pygame.K_RIGHT]: # right key
-         #   self.x += dist # move right
-        #elif key[pygame.K_LEFT]: # left key
-         #   self.x -= dist # move left
-def magnitude(v):
-    return math.sqrt(sum(v[i]*v[i] for i in range(len(v))))
+            return data[key]
 
-def sub(u, v):
-    return [u[i]-v[i] for i in range(len(u))]
+    def setMemory(key, value):
+        with open("finalfight_lib/memory.json", "r+") as jsonFile:
+            data = json.load(jsonFile)
 
-def normalize(v):
-    return [v[i]/magnitude(v)  for i in range(len(v))]
+            data[key] = value
+
+            jsonFile.seek(0)  # rewind
+            json.dump(data, jsonFile)
+            jsonFile.truncate()
+
+    def loadScore(self,screen):
+        font = pygame.font.Font("data/finalfight/FEASFBRG.ttf", 30)
+        score = "score: %d" % Score.getMemory("score")
+        self.temp_surface = font.render(score, 1, (255, 255, 255))
+
+    def blitScore(self,screen):
+        screen.blit(self.temp_surface, (1150, 10))
+
+class Pause(Game):
+    def __init__(self, screen):
+        Game.__init__(self, screen)
+        RUNNING, PAUSE = 0, 1
+        self.state = RUNNING
+
+
+    def magnitude(v):
+        return math.sqrt(sum(v[i] * v[i] for i in range(len(v))))
+
+    def sub(u, v):
+        return [u[i] - v[i] for i in range(len(u))]
+
+    def normalize(v):
+        return [v[i] / magnitude(v) for i in range(len(v))]
+
+    def loadPause(self):
+        self.pause_text = pygame.font.Font("finalfight_lib/FEASFBRG.ttf", 60).render('Paused', True,
+                                                                                 pygame.color.Color('White'))
+        s = pygame.Surface((1280, 720), pygame.SRCALPHA)  # per-pixel alpha
+        s.fill((0, 0, 0, 150))
+
 
 class run():
     def runm(self):
@@ -63,55 +160,55 @@ class run():
 
         pygame.display.set_caption("Final Fight")
 
-        forrestImage = pygame.image.load("data/finalfight/openplek.png").convert()
-        forrestImage = pygame.transform.scale(forrestImage,(width,height))
-        screen.blit(forrestImage,(0,0))
 
-        #rescale = 2
-        boss = pygame.image.load("data/finalfight/boss2.png").convert_alpha()
-        bossWidth = boss.get_rect().width
-        bossHeight = boss.get_rect().height
-        boss = pygame.transform.scale(boss,(bossWidth,bossHeight))
-        screen.blit(boss,(520,300))
+        newPlayer = Player(screen)
+        newBoss = Boss(screen)
+        newWolk = Wolk(screen)
+        background = Background(screen)
+        newScore = Score(screen)
+        newPause = Pause(screen)
 
-        wolk = pygame.image.load("data/finalfight/spreekwolk.png").convert_alpha()
-        #wolkWidth = wolk.get_rect().width
-        #wolkHeight = wolk.get_rect().height
-        wolk = pygame.transform.scale(wolk,(350,200))
-        screen.blit(wolk,(590,130))
+        background.loadForrest("data/finalfight/openplek.png")
 
-        player = pygame.image.load("data/finalfight/monkey.png").convert_alpha()
-        #playerWidth = player.get_rect().width
-        #playerHeight = player.get_rect().height
-        player = pygame.transform.scale(player,(150,150))
-        screen.blit(player,(300,550))
+        newPlayer.loadPlayer("data/finalfight/monkey.png")
 
+        #newPlayer.movePlayer()
 
-        pygame.font.init()
-        font = pygame.font.Font("data/finalfight/FEASFBRG.ttf", 30)
-        score = "score: %d" % getMemory("score")
-        temp_surface = font.render(score, 1, (255,255,255))
-        screen.blit(temp_surface,(1150,10))
+        #newPlayer.draw(screen)
 
-        setMemory("score", 768)
+        newBoss.loadBoss("data/finalfight/boss2.png")
 
-        pygame.init()
+        newWolk.loadWolk("data/finalfight/spreekwolk.png")
 
-        screen = pygame.display.set_mode((width, height))
-        #clock = pygame.time.Clock()
+        newScore.loadScore(screen)
 
+        newPause.loadPause()
 
-        pause_text = pygame.font.Font("data/finalfight/FEASFBRG.ttf", 60).render('Paused', True, pygame.color.Color('White'))
-        s = pygame.Surface((width, height), pygame.SRCALPHA)  # per-pixel alpha
-        s.fill((0, 0, 0, 150))
+        background.blitForrest()
+
+        newWolk.blitwolk(screen)
+
+        newBoss.blitBoss(screen)
+
+        newPlayer.blitPlayer()
+
+        newScore.blitScore(screen)
+
+        Score.setMemory("score", 768)
+
+        clock = pygame.time.Clock()
 
         RUNNING, PAUSE = 0, 1
         state = RUNNING
 
-        #player = Player()  # create an instance
-        clock = pygame.time.Clock()
+        pause_text = pygame.font.Font("finalfight_lib/FEASFBRG.ttf", 60).render('Paused', True,
+                                                                                         pygame.color.Color('White'))
+        s = pygame.Surface((width, height), pygame.SRCALPHA)  # per-pixel alpha
+        s.fill((0, 0, 0, 150))
+
 
         while True:
+
             for e in pygame.event.get():
                 if e.type == pygame.QUIT: break
                 if e.type == pygame.KEYDOWN:
@@ -121,28 +218,42 @@ class run():
                 screen.fill((0, 0, 0))
 
                 if state == RUNNING:
-                    screen.blit(forrestImage, (0, 0))
-                    screen.blit(boss, (520, 300))
-                    screen.blit(wolk, (590, 130))
-                    screen.blit(player, (300, 550))
-                    screen.blit(temp_surface, (1150, 10))
+                    background.blitForrest()
+                    newWolk.blitwolk(screen)
+                    newBoss.blitBoss(screen)
+                    newPlayer.blitPlayer()
+                    newScore.blitScore(screen)
+
 
 
 
                 elif state == PAUSE:
-
-                    screen.blit(forrestImage, (0, 0))
-                    screen.blit(boss, (520, 300))
-
-                    screen.blit(player, (300, 550))
-                    screen.blit(temp_surface, (1150, 10))
+                    background.blitForrest()
+                    #newWolk.blitwolk()
+                    newBoss.blitBoss(screen)
+                    newPlayer.blitPlayer()
+                    newScore.blitScore(screen)
                     screen.blit(s, (0, 0))
                     screen.blit(pause_text, (600, 360))
-            #player.handle_keys()  # handle the keys
+
+
 
                 pygame.display.flip()
                 clock.tick(60)
                 continue
+            #player.handle_keys()  # handle the keys
+
             break
 
+
+        #key = pygame.key.get_pressed()
+         #   dist = 1  # distance moved in 1 frame, try changing it to 5
+          #  if key[pygame.K_DOWN]:  # down key
+           #     newPlayer.movePlayer() # move down
+            #elif key[pygame.K_UP]:  # up key
+             #   newPlayer.movePlayer()  # move up
+            #if key[pygame.K_RIGHT]:  # right key
+             #   newPlayer.movePlayer()  # move right
+            #elif key[pygame.K_LEFT]:  # left key
+             #   newPlayer.movePlayer()  # move left
 
--- finalfight_lib/test2.py
+++ finalfight_lib/test2.py
@@ -1,49 +1,62 @@
-import pygame, math, itertools
-
-def magnitude(v):
-    return math.sqrt(sum(v[i]*v[i] for i in range(len(v))))
-
-def sub(u, v):
-    return [u[i]-v[i] for i in range(len(u))]
-
-def normalize(v):
-    return [v[i]/magnitude(v)  for i in range(len(v))]
-
-pygame.init()
-screen = pygame.display.set_mode((300, 300))
-clock = pygame.time.Clock()
-
-path = itertools.cycle([(26, 43), (105, 110), (45, 225), (145, 295), (266, 211), (178, 134), (250, 56), (147, 12)])
-target = next(path)
-ball, speed = pygame.rect.Rect(target[0], target[1], 10, 10), 3.6
-pause_text = pygame.font.SysFont('Consolas', 32).render('Pause', True, pygame.color.Color('White'))
-
-RUNNING, PAUSE = 0, 1
-state = RUNNING
-
-while True:
-    for e in pygame.event.get():
-        if e.type == pygame.QUIT: break
-        if e.type == pygame.KEYDOWN:
-            if e.key == pygame.K_p: state = PAUSE
-            if e.key == pygame.K_s: state = RUNNING
-    else:
-        screen.fill((0, 0, 0))
-
-        if state == RUNNING:
-            target_vector = sub(target, ball.center)
-
-            if magnitude(target_vector) < 2:
-                target = next(path)
-            else:
-                ball.move_ip([c * speed for c in normalize(target_vector)])
-
-            pygame.draw.rect(screen, pygame.color.Color('Yellow'), ball)
-
-        elif state == PAUSE:
-            screen.blit(pause_text, (100, 100))
-
-        pygame.display.flip()
-        clock.tick(60)
-        continue
-    break
+''' pg_sprite_keys1.py
+move a sprite rect with the arrow keys
+see also ...
+http://www.pygame.org/docs/ref/sprite.html
+http://www.pygame.org/docs/ref/time.html
+'''
+import pygame as pg
+pg.init()
+# set screen size
+width = 640
+height = 480
+screen = pg.display.set_mode((width, height))
+pg.display.set_caption("move with arrow keys (escape key to exit)")
+# color (r, g, b) tuple, values 0 to 255
+white = (255, 255, 255)
+background = pg.Surface(screen.get_size())
+background.fill(white)
+# pick an image file you have in the working folder
+# or use full path
+# load image of sprite
+sprite = pg.image.load("monkey.png")
+sprite = pg.transform.scale(sprite,(150,150))
+# get image space, rectangle ulc, lrc coordinates
+sprite_rect = sprite.get_rect()
+# test
+print(sprite_rect)  # <rect(0, 0, 64, 64)>
+# initial position of sprite, center of screen
+sprite_rect.centerx = (width//2)
+sprite_rect.centery = (height//2)
+screen.blit(background, (0, 0))
+screen.blit(sprite, sprite_rect)
+pg.display.flip()
+clock = pg.time.Clock()
+# create the obligatory event loop
+while 1:
+    # limit runtime speed to 30 frames/second
+    clock.tick(30)
+    pg.event.pump()
+    # a key has been pressed
+    keyinput = pg.key.get_pressed()
+    # press escape key to quit game
+    if keyinput[pg.K_ESCAPE]:
+        raise SystemExit
+    # optional exit on window corner x click
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            raise SystemExit
+    # check arrow keys
+    # move sprite in direction of arrow by 2 pixels
+    if keyinput[pg.K_LEFT]:
+        sprite_rect.centerx -= 2
+    elif keyinput[pg.K_RIGHT]:
+        sprite_rect.centerx += 2
+    elif keyinput[pg.K_UP]:
+        sprite_rect.centery -= 2
+    elif keyinput[pg.K_DOWN]:
+        sprite_rect.centery += 2
+    screen.blit(background, (0,0))
+    screen.blit(sprite, sprite_rect)
+    # update display
+    pg.display.flip()
