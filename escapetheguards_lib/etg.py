a0928cf00f18c5076d3d817effa6e171fe8bfd4a
Wesley9942 <31687129+0946241@users.noreply.github.com>
2018-01-08 10:03:46 +0100

    changes

--- escapetheguards_lib/etg.py
+++ escapetheguards_lib/etg.py
@@ -8,22 +8,87 @@ from escapetheguards_lib import config
 
 pygame.init()
 
+# Set top left text in screen
+pygame.display.set_caption('Escape The Guards')
+# Set up clock for fps count.
+clock = pygame.time.Clock()
+# Different global values defined.
+pause = False
+x_player = (200)
+y_player = (300)
+
+### LOADING THE IMAGES: ###
+
+backgroundintro = pygame.image.load('data/escapetheguards/bgintro.png').convert()
+monkeyImg = pygame.image.load('data/escapetheguards/monkey.png').convert_alpha()
+logoImg = pygame.image.load('data/escapetheguards/logo.png')
+tableImg = pygame.image.load('data/escapetheguards/picknicktable.png').convert_alpha()
+cageImg = pygame.image.load('data/escapetheguards/cage.png').convert_alpha()
+cage2Img = pygame.image.load('data/escapetheguards/cage2.png').convert_alpha()
+cage3Img = pygame.image.load('data/escapetheguards/cage3.png').convert_alpha()
+cagedoorImg = pygame.image.load('data/escapetheguards/cagedoor.png').convert_alpha()
+bananaImg = pygame.image.load('data/escapetheguards/bananapoint.png').convert_alpha()
+guard_rightImg = pygame.image.load('data/escapetheguards/guards_right.png').convert_alpha()
+guard_leftImg = pygame.image.load('data/escapetheguards/guards_left.png').convert_alpha()
+guard_topImg = pygame.image.load('data/escapetheguards/guards_top.png').convert_alpha()
+guard_bottomImg = pygame.image.load('data/escapetheguards/guards_bottom.png').convert_alpha()
+circlesign = pygame.image.load('data/escapetheguards/circleSign.png').convert_alpha()
+exitgame = pygame.image.load('data/escapetheguards/exitGame.png').convert_alpha()
+exitgamehover = pygame.image.load('data/escapetheguards/exitGameHover.png').convert_alpha()
+pausegame = pygame.image.load('data/escapetheguards/pauseGame.png').convert_alpha()
+pausegamehover = pygame.image.load('data/escapetheguards/pauseGameHover.png').convert_alpha()
+playgame = pygame.image.load('data/escapetheguards/playGame.png').convert_alpha()
+playgamehover = pygame.image.load('data/escapetheguards/playGameHover.png').convert_alpha()
+sign = pygame.image.load('data/escapetheguards/sign.png').convert_alpha()
+acid = pygame.image.load("data/escapetheguards/acid_monkey.png").convert_alpha()
+apprentice = pygame.image.load("data/escapetheguards/apprentice_monkey_top.png").convert_alpha()
+default = pygame.image.load("data/escapetheguards/default_monkey_top.png").convert_alpha()
+dragon = pygame.image.load("data/escapetheguards/dragon_monkey_top.png").convert_alpha()
+engineer = pygame.image.load("data/escapetheguards/engineer_monkey_top.png").convert_alpha()
+farmer = pygame.image.load("data/escapetheguards/farmer_monkey.png").convert_alpha()
+ninja = pygame.image.load("data/escapetheguards/ninja_monkey_top.png").convert_alpha()
+robo = pygame.image.load("data/escapetheguards/robo_monkey_top.png").convert_alpha()
+roboflat = pygame.image.load("data/escapetheguards/robo_monkey.png").convert_alpha()
+superMonkey = pygame.image.load("data/escapetheguards/super_monkey_top.png").convert_alpha()
+mainImg = pygame.image.load('data/escapetheguards/monkey1.png').convert_alpha()
+charwFlagImg = pygame.image.load('data/escapetheguards/charwflag.png').convert_alpha()
+flagImg = pygame.image.load("data/escapetheguards/flag.png").convert_alpha()
+noFlagImg = pygame.image.load("data/escapetheguards/noflag.png").convert_alpha()
+targetImg = pygame.image.load("data/escapetheguards/target.png").convert_alpha()
+
+
+
+# Transforming Characters
+acid = pygame.transform.scale(acid, (70,70))
+apprentice = pygame.transform.scale(apprentice, (70,70))
+default = pygame.transform.scale(default, (70,70))
+pygame.transform.scale(dragon, (70,70))
+engineer = pygame.transform.scale(engineer, (70,70))
+farmer = pygame.transform.scale(farmer, (70,70))
+ninja = pygame.transform.scale(ninja, (70,70))
+robo = pygame.transform.scale(robo, (70,70))
+acid = pygame.transform.scale(acid, (150, 300))
+backgroundintro = pygame.transform.scale(backgroundintro, (config.screen_width, config.screen_height))
+bananaImg = pygame.transform.scale(bananaImg, (30,30))
+exitgame = pygame.transform.scale(exitgame, (270,135))
+exitgamehover = pygame.transform.scale(exitgamehover, (270,135))
+cage2Img = pygame.transform.scale(cage2Img, (260,250))
+cage3Img = pygame.transform.scale(cage3Img, (250,200))
+guard_rightImg = pygame.transform.scale(guard_rightImg, (100,100))
+monkeyImg = pygame.transform.scale(monkeyImg, (500, 600))
+guard_leftImg = pygame.transform.scale(guard_leftImg, (400,250))
+guard_bottomImg = pygame.transform.scale(guard_bottomImg, (100,100))
+guard_topImg = pygame.transform.scale(guard_topImg, (280, 350))
+noFlagImg = pygame.transform.scale(noFlagImg, (70,70))
+
 
 
 
 class run():
     def runm(self):
-        # Set top left text in screen
-        pygame.display.set_caption('Escape The Guards')
 
-        # Set up clock for fps count.
-        clock = pygame.time.Clock()
-        # Different global values defined.
-        pause = False
-        x_player = (200)
-        y_player = (300)
 
-        def back():
+        def back(self):
             mymenu = slidemenu.run()
             mymenu.runm()
 
@@ -52,7 +117,7 @@ class run():
             else:
                 config.screen.blit(iimg, (x, y, w, h))
 
-        def spot():
+        def spot(self):
             dodged = 0
             banana = 0
             things_dodged(dodged)
@@ -81,19 +146,17 @@ class run():
 
                 pygame.draw.rect(config.screen, config.brown, [0, 690, 750, 30])  # Bottom row!!
 
-                config.screen.blit(config.cage2Img, (30, 0))
-                config.screen.blit(config.cage3Img, (30, 490))
-                config.screen.blit(config.cage3Img, (400, 490))
-                config.screen.blit(config.cagedoorImg, (290, 70))
-                config.screen.blit(config.tableImg, (600, 200))
+                config.screen.blit(cage2Img, (30, 0))
+                config.screen.blit(cage3Img, (30, 490))
+                config.screen.blit(cage3Img, (400, 490))
+                config.screen.blit(cagedoorImg, (290, 70))
+                config.screen.blit(tableImg, (600, 200))
 
-                config.screen.blit(config.guard_leftImg, (550, 30))
-                config.screen.blit(config.guard_topImg, (230, 320))
+                config.screen.blit(guard_leftImg, (550, 30))
+                config.screen.blit(guard_topImg, (230, 320))
 
-                config.screen.blit(config.logoImg, (1010, 350))
-                config.screen.blit(config.bananaImg, (300, 350))
-                config.screen.blit(config.bananaImg, (600, 620))
-                config.screen.blit(config.bananaImg, (900, 300))
+                config.screen.blit(logoImg, (1010, 350))
+
 
                 things_dodged(dodged)
                 banana_count(banana)
@@ -107,8 +170,8 @@ class run():
                 # button("Back to Intro", 590, 350, 100, 50, config.black, config.yellow, "leave")
                 # button("Start Game", 590, 550, 100, 50, config.black, config.yellow, "level1")
 
-                button("Lets Play", 450, 400, 20, 20, config.playgame, config.playgamehover, level_1)
-                button("Back to menu", 600, 380, 100, 50, config.exitgame, config.exitgamehover, game_intro)
+                button("Lets Play", 450, 400, 20, 20, playgame, playgamehover, level_1)
+                button("Back to menu", 600, 380, 100, 50, exitgame, exitgamehover, game_intro)
 
                 # button("Play Again!", 590, 350, 100, 50, config.yellow, config.light_black, level_1())
 
@@ -134,17 +197,17 @@ class run():
                 pygame.display.update()
                 clock.tick(15)
 
-        def unpause():
+        def unpause(self):
             global pause
             pause = False
 
-        def paused():
+        def paused(self):
 
             pause = True
             dodged = 0
             banana = 0
 
-            s = pygame.Surface((config.screen_width, config.screen_height), pygame.SRCALPHA)
+            s = pygame.Surface((screen_width, screen_height), pygame.SRCALPHA)
             s.fill((0, 0, 0, 150))
 
             while pause:
@@ -167,10 +230,10 @@ class run():
 
                 pygame.draw.rect(config.screen, config.brown, [0, 690, 750, 30])  # Bottom row!!
 
-                config.screen.blit(config.cage2Img, (30, 0))
-                config.screen.blit(config.cage3Img, (30, 490))
-                config.screen.blit(config.cage3Img, (400, 490))
-                config.screen.blit(config.cagedoorImg, (290, 70))
+                config.screen.blit(cage2Img, (30, 0))
+                config.screen.blit(cage3Img, (30, 490))
+                config.screen.blit(cage3Img, (400, 490))
+                config.screen.blit(cagedoorImg, (290, 70))
                 config.screen.blit(config.tableImg, (600, 200))
 
                 config.screen.blit(config.guard_leftImg, (550, 30))
@@ -201,7 +264,7 @@ class run():
                 pygame.display.update()
                 clock.tick(15)
 
-        def game_intro():
+        def game_intro(self):
             global pause
             intro = True
 
@@ -356,10 +419,10 @@ class run():
         # balance = getMemory("balance")
         # monkey = getMemory("bought")
 
-        def things(thingx, thingy, thingw, thingh, color):
+        def things(self, thingx, thingy, thingw, thingh, color):
             pygame.draw.rect(config.screen, color, [thingx, thingy, thingw, thingh])
 
-        def message_display(text):
+        def message_display(self, text):
             largeText = pygame.font.Font('freesansbold.ttf', 115)
             TextSurf, TextRect = text_objects(text, largeText)
             TextRect.center = ((config.screen_width / 2), (config.screen_height / 2))
@@ -369,7 +432,7 @@ class run():
 
             time.sleep(2)
 
-        def face_level1():
+        def face_level1(self):
             gameExit = False
             dodged = 0
             banana = 0
@@ -435,7 +498,7 @@ class run():
                 pygame.display.update()
                 config.clock.tick(60)
 
-        def level_1():
+        def level_1(self):
 
             global pause
 
@@ -563,9 +626,9 @@ class run():
                 # print('screen is updated!')
                 config.clock.tick(60)
 
-class runm():
-    game_intro()
-    level_1()
-    paused()
+class runm(self):
+    self.game_intro()
+    self.level_1()
+    self.paused()
     pygame.quit()
     sys.quit()
